c451c093a0580a68a7549838e7f13c460ca90e93
Shittu24 <ibrahimshittu247@gmail.com>
2023-01-03 12:25:23 -0800

    minor change

--- src/client.js
+++ src/client.js
@@ -2,10 +2,10 @@ import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    projectId: "zb4l0zpb",
     dataset: "production",
     apiVersion:"2022-02-01",
-    token: process.env.REACT_APP_SANITY_TOKEN,
+    token: "skeXs3xa0g2gqshLt2Udr2m1OLpRyNa7qyiyqs62S1D53rmVkJ1rPOfRC7xJMszINZSS5PjldtzTMfyBxqKWPHb9OvQxR2tdmZsKoUsvUgFMyTlxoe8bq3uOEdxy7RWi43WJLsHoa4IzykqGlOrb6hd3rnefSxwHEfYNckaLR4WjoKCBWyQC",
     useCdn: true
 })
 
